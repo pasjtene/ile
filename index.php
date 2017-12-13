0050cca7d80eb6263a8714bc709dba6d5a7e8dba
pasjtene <pasjtene@yahoo.co.uk>
2017-12-13 12:28:34 +0000

    utf8

--- index.php
+++ index.php
@@ -25,56 +25,56 @@ require 'navbar.html';
             <div class="category-title">
                 <h3><a href="/ile/Views/anglais_old.html">COURS D'ANGLAIS POUR ADULTES</a></h3>
             </div>
-            Il s’agit ici de cours de langue généraux des niveaux A1-A2 pour adultes. Les objets et contenus d’apprentissages sont conçus d’après le Cadre européen commun de référence pour les langues (CECR).
+            Il sâ€™agit ici de cours de langue gÃ©nÃ©raux des niveaux A1-A2 pour adultes. Les objets et contenus dâ€™apprentissages sont conÃ§us dâ€™aprÃ¨s le Cadre europÃ©en commun de rÃ©fÃ©rence pour les langues (CECR).
         </div>
         <div class="categorie">
             <div class="category-title">
-                <h3><a href="/ile/Views/anglais_old.html">COURS D’ANGLAIS POUR ENFANTS ET JEUNES</a></h3>
+                <h3><a href="/ile/Views/anglais_old.html">COURS Dâ€™ANGLAIS POUR ENFANTS ET JEUNES</a></h3>
             </div>
-            Ces cours d’adressent aux jeunes Lycéens ou tout apprenant âgé entre 13 et 18 ans. Les élèves disposant de connaissances préalables ou débutants peuvent s’inscrire.
+            Ces cours dâ€™adressent aux jeunes LycÃ©ens ou tout apprenant Ã¢gÃ© entre 13 et 18 ans. Les Ã©lÃ¨ves disposant de connaissances prÃ©alables ou dÃ©butants peuvent sâ€™inscrire.
         </div>
         <div class="categorie">
             <div class="category-title">
-                <h3><a href="/ile/Views/anglais_old.html">COURS DE PRÉPARATION AUX EXAMENS</a></h3>
+                <h3><a href="/ile/Views/anglais_old.html">COURS DE PRÃ‰PARATION AUX EXAMENS</a></h3>
             </div>
 
-            Ces cours préparent aux examens TOEFL et IELTS et permettent de connaître le format de l’examen.
+            Ces cours prÃ©parent aux examens TOEFL et IELTS et permettent de connaÃ®tre le format de lâ€™examen.
         </div>
         <div class="categorie">
             <div class="category-title">
                 <h3><a href="/ile/Views/anglais_old.html">Cours aux particuliers</a></h3>
             </div>
-            Cours basés sur des contenus particuliers, destinés à des groupes-cibles spéciaux ou pour l’exercice d’aptitudes uniques:
+            Cours basÃ©s sur des contenus particuliers, destinÃ©s Ã  des groupes-cibles spÃ©ciaux ou pour lâ€™exercice dâ€™aptitudes uniques:
         </div>
         <h3>INFORMATIONS</h3>
         <div class="info-container">
             <div class="information">
                 <div class="info-title"><h3><a href="/ile/Views/dates.html">DATES ET TARIFS</a></h3></div>
-                Tous les cours de Yaoundé
+                Tous les cours de YaoundÃ©
             </div>
             <div class="information">
                 <div class="info-title"><h3><a href="#">CONTACT ET INSCRIPTION</a></h3></div>
-                Vous êtes intéressé(e) par nos cours de langue ou avez des questions ? Nous vous renseignons volontiers.
+                Vous Ãªtes intÃ©ressÃ©(e) par nos cours de langue ou avez des questions ? Nous vous renseignons volontiers.
             </div>
             <div class="information">
                 <div class="info-title"><h3><a href="#">NOTRE CONCEPT D'APPRENTISSAGE</a></h3></div>
 
-                Des progrès rapides dans votre apprentissage sont garantis par des enseignants hautement qualifiés, des méthodes d’enseignement modernes, un accompagnement et un soutien soutenu ainsi qu’un système de niveaux de cours reconnu mondialement.
+                Des progrÃ¨s rapides dans votre apprentissage sont garantis par des enseignants hautement qualifiÃ©s, des mÃ©thodes dâ€™enseignement modernes, un accompagnement et un soutien soutenu ainsi quâ€™un systÃ¨me de niveaux de cours reconnu mondialement.
             </div>
         </div>
 
-        <div>Accédez en un clic à l’<a href="Views/inscription.html">espace candidature</a></div>
+        <div>AccÃ©dez en un clic Ã  lâ€™<a href="Views/inscription.html">espace candidature</a></div>
     </div>
     <div class="flex2">
         <div class="hexagon">
             <P class="titre_hexa">PROGRAMMES BTC-SUP</P>
             <hr>
-            <div class="hexag_text"><a href="">PANORAMA DES ÉTUDES</a></div>
+            <div class="hexag_text"><a href="">PANORAMA DES Ã‰TUDES</a></div>
             <div class="hexag_text"><a href="">BACHELOR INFORMATIQUE BAC+3</a></div>
-            <div class="hexag_text"><a href="">INGÉNIERIE INFORMATIQUE BAC+5</a></div>
+            <div class="hexag_text"><a href="">INGÃ‰NIERIE INFORMATIQUE BAC+5</a></div>
             <div class="hexag_text"><a href="">LES TITRES ET DIPLOMES</a></div>
-            <div class="hexag_text"><a href="">LES OUTILS NUMÉRIQUES</a></div>
-            <div class="hexag_text"><a href="">LES SPÉCIALISATIONS</a></div>
+            <div class="hexag_text"><a href="">LES OUTILS NUMÃ‰RIQUES</a></div>
+            <div class="hexag_text"><a href="">LES SPÃ‰CIALISATIONS</a></div>
             <hr>
         </div>
     </div>
