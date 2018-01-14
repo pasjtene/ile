8b51c2b3da2f53a69f0b5b836f76e88dbd96f663
pasjtene <pasjtene@yahoo.co.uk>
2018-01-14 18:21:50 +0000

    mobile js

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -8,6 +8,8 @@ synthesis.documentReady(function(){
 */
 $(document).ready(function(){
    console.log("jquery is running");
+
+
     $("#sous-titre-anglais").slideDown();
     $("#left-nav-al-cont").slideDown();
     $("#sous-titre-lef-nav-an").click(function(e){
@@ -53,4 +55,54 @@ $(document).ready(function(){
     });
 
 
+    // for mobile
+
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    console.log("The wi is: "+width);
+
+    if(detectmob()) {
+        console.log("This is a mobile phone:++ 2");
+    }
+
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        // some code..
+        console.log("This is a mobile phone:++ 1");
+    }
+
+    var isMobile = window.matchMedia("and (max-device-width : 667px");
+
+    if (isMobile.matches) {
+        //Conditional script here
+        console.log("This is a mobile phone:++ ");
+    }
+
+    $(window).on("resize", function (e) {
+        checkScreenSize();
+    });
+
+    checkScreenSize();
+
+    function checkScreenSize(){
+        var newWindowWidth = $(document).width();
+        if (newWindowWidth < 481) {
+            console.log("This is a mobile phone: "+newWindowWidth);
+            alert("This is a mobile phone");
+            $(".fcontent").hide();
+        }
+        else
+        {
+            $(".fcontent").show();
+            console.log("This is NOT a mobile phone: "+newWindowWidth);
+        }
+    }
+
+
 });
+
+function detectmob() {
+    if(window.innerWidth <= 800 && window.innerHeight <= 600) {
+        return true;
+    } else {
+        return false;
+    }
+}
